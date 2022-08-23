d23ca4b269941667efe3accfdf77caaf81cf9f4e
gcamarg <gcamargo@outlook.com.br>
2022-08-22 21:12:12 -0300

    Removing spring security config from gitignore

---
+++ src/main/java/com/camargo/bullshorn/security/configuration/WebSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.camargo.bullshorn.security.configuration;
+
+
+import com.camargo.bullshorn.appuser.AppUserService;
+import com.camargo.bullshorn.security.filter.JWTValidationFilter;
+import com.camargo.bullshorn.security.filter.UserAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Value("${jwt.secret}")
+    private String TOKEN_SECRET;
+    @Value("${jwt.expiration}")
+    private int TOKEN_EXPIRATION;
+    @Value("${client.url}")
+    private String CLIENT_URL;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/registration/**", "/login", "/api/v1/marketdata/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .addFilter(new UserAuthenticationFilter(authenticationManager(),TOKEN_SECRET, TOKEN_EXPIRATION))
+                .addFilter(new JWTValidationFilter(authenticationManager(),TOKEN_SECRET));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(CLIENT_URL));
+        configuration.setAllowedMethods(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
