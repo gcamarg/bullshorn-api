13636f78d21946a87ef443b7d2e0bef230c0b00e
gcamarg <gcamargo@outlook.com.br>
2022-08-29 05:18:18 -0300

    changed scripts default user to ubuntu

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/start_service.sh
-chmod +x /home/ec2-user/server/stop_service.sh
+chmod +x /home/ubuntu/server/* jar
+chmod +x /home/ubuntu/server/start_service.sh
+chmod +x /home/ubuntu/server/stop_service.sh
--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Starting the application"
-cd /home/ec2-user/server
+cd /home/ubuntu/server
 sudo java -jar -Dserver.port=80 *.jar > /dev/null 2> /dev/null < /dev/null &
