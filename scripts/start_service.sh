08d51994377cf4396dc2b7adbb3299773b8fc7f7
gcamarg <gcamargo@outlook.com.br>
2022-09-02 03:45:27 -0300

    Getting properties from secrets manager

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 echo "Starting the application"
 cd /home/ubuntu/server
-database_password="$database_password"
-database_user="$database_user"
-emailSender_address="$emailSender_address"
-emailSender_password="$emailSender_password"
-token_secret="$token_secret"
-finnhub_token="$finnhub_token"
-client_domain="$client_domain"
 sudo java -jar -Dserver.port=80 bullshorn-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
